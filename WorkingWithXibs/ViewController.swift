7ccfe65c887d87fad8bd17f9a5a1aad7d7a35fa5
Michael Amiro <mikeamiro@digitallegion.co.ke>
2019-07-01 13:10:46 +0300

    Initial Commit

---
+++ WorkingWithXibs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WorkingWithXibs
+//
+//  Created by Michael Amiro on 01/07/2019.
+//  Copyright © 2019 Michael Amiro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
